516a32bf4e4cf4556837430a6397ba331a9d7881
Max Bruchmann <bruchmann@signal7.de>
2015-06-29 20:23:42 +0200

    Added server start

--- src/main/java/zochollot/Main.java
+++ src/main/java/zochollot/Main.java
@@ -1,7 +1,13 @@
 package zochollot;
+
+import org.eclipse.jetty.server.Server;
+
 public class Main {
     
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+	
+		Server server = new Server(8888); //create server on port 8888 (http://localhost:8888/)
+		server.start();//start server process
+		server.join(); //join (wait) on server process
 	}
 }
